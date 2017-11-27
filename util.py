Fix hard mask calculation

util.py
@@ -71,7 +71,7 @@ def ComputeMask(input_mag,unet_model="unet.model",hard=True):
     mask = unet(input_mag[np.newaxis,np.newaxis,1:,:]).data[0,0,:,:]
     mask = np.vstack((np.zeros(mask.shape[1],dtype="float32"),mask))
     if hard:
-        hard_mask = np.zeros((mask.shape[0]+1,mask.shape[1]),dtype="float32")
+        hard_mask = np.zeros(mask.shape,dtype="float32")
         hard_mask[mask>0.5] = 1
         return hard_mask
     else: